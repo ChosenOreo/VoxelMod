4e24582479d442f447faeb1b763ef012fb031915
Adrian Collado <acollado@citlink.net>
2013-11-26 20:31:54 -0500

    Added AbstractCamera.cs.

---
+++ SGE/Camera/AbstractCamera.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK;
+
+namespace SGE.Camera {
+    public abstract class AbstractCamera : ICamera {
+        #region Properties
+        /// <summary>
+        /// Gets the current View Matrix defined by the camera's
+        /// target and position vectors.
+        /// </summary>
+        Matrix4 ViewMatrix {
+            get {
+                return _ViewMatrix;
+            }
+        }
+        #endregion
+
+        #region Private Fields
+        private Matrix4 _ViewMatrix;
+        #endregion
+    }
+}
